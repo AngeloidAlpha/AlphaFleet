7dd0e377c47367e99b4e130fab77541b4f57f422
AngeloidAlpha <angeloid_alpha@abv.bg>
2026-02-16 17:05:59 +0200

    Entire project theme changed to dark

--- AlphaFleet/Controllers/HomeController.cs
+++ AlphaFleet/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace AlphaFleet.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
