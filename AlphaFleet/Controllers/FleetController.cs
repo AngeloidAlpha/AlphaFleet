797cb94d43c63a57c9f8bc31c0d1932a62f45d0f
AngeloidAlpha <angeloid_alpha@abv.bg>
2026-04-07 14:49:04 +0300

    Updated FleetControler with Redirects to current user fleet or to Create if they don't have one. Updated fleet to include User. Added Create view.

--- AlphaFleet/Controllers/FleetController.cs
+++ AlphaFleet/Controllers/FleetController.cs
@@ -1,6 +1,8 @@
 ﻿using AlphaFleet.Data.Models;
 using AlphaFleet.Services;
+using AlphaFleet.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlphaFleet.Controllers
@@ -9,10 +11,12 @@ namespace AlphaFleet.Controllers
     public class FleetController : Controller
     {
         private readonly IFleetService _fleetService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FleetController(IFleetService fleetService)
+        public FleetController(IFleetService fleetService, UserManager<ApplicationUser> userManager)
         {
             _fleetService = fleetService;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -31,6 +35,52 @@ namespace AlphaFleet.Controllers
                 return this.View("BadRequest");
             }
             return this.View(fleet);
+        }
+        [HttpGet]
+        public async Task<IActionResult> MyFleet()
+        {
+            string userId = _userManager.GetUserId(User);
+            Fleet? fleet = await _fleetService.GetUserFleetAsync(userId);
+            if (fleet == null)
+            {
+                return RedirectToAction("Create");
+            }
+            return RedirectToAction("Details", new { id = fleet.Id });
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            string userId = _userManager.GetUserId(User)!;
+            Fleet? existingFleet = await _fleetService.GetUserFleetAsync(userId);
+            if (existingFleet != null)
+            {
+                TempData["FleetError"] = "You already command a fleet. A commander can only lead one fleet.";
+                return RedirectToAction("MyFleet");
+            }
+            return View(new FleetFormViewModel());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FleetFormViewModel model)
+        {
+            string userId = _userManager.GetUserId(User)!;
+            Fleet? existingFleet = await _fleetService.GetUserFleetAsync(userId);
+            if (existingFleet != null)
+            {
+                TempData["FleetError"] = "You already command a fleet. A commander can only lead one fleet.";
+                return RedirectToAction("MyFleet");
+            }
+            if (!ModelState.IsValid)
+                return View(model);
+            var fleet = new Fleet
+            {
+                Name = model.Name,
+                Location = model.Location,
+                UserId = userId,
+            };
+
+            await _fleetService.CreateFleetAsync(fleet);
+            return RedirectToAction("Details", new { id = fleet.Id });
         }
     }
 }
